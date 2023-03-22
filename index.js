685dea74a057d12ced66197768df4d32d07b2dab
nathan <nathanmageka6@gmail.com>
2023-03-22 15:31:29 +0300

    nathan

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers() {
+    return['Antonia', 'Nuru']
+}
+function returnLastTwoDrivers() {
+    return["Amari", "Mo"]
+}
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers]
+const fareMultipier=createFareMultiplier
+function createFareMultiplier(createFareMultiplier){
+    return fareMultipier
+}
+function createFareMultiplier(num1){
+    return function(num1){
+        return num1*num1
+    }
+}
+const fareDoubler=(num1)=>num1*2;
+const fareTripler=(num1)=>num1*3;
+function selectDifferentDrivers(arrayOfDrivers){
+    return ['Antonia', 'Nuru',]; function selectDifferentDrivers(){
+        return["Amari", "Mo"]
+    }
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
